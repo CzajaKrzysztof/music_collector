4f14ae390239d99662630ac31fc15b12f3949fd6
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-14 11:39:08 +0100

    Add functionality to import data from file

--- music_collector.py
+++ music_collector.py
@@ -2,7 +2,8 @@ import music_reports
 import display
 
 def main():
-
+    albums_list = music_reports.import_data_from_file()
+    print(albums_list)
 
 if __name__ == '__main__':
     main()
--- music_reports.py
+++ music_reports.py
@@ -1,5 +1,9 @@
-def main():
+def import_data_from_file(filename='text_albums_data.txt'):
+    albums_dictionary = {}
+    content = []
+    key = 1
+    with open(filename, 'r') as my_file:
+        for line in my_file:
+            content.append(line.rstrip('\n').split(','))
 
-
-if __name__ == '__main__':
-    main()
+    return content
