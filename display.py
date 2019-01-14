be31c618ec2b7291ff71c3a07c32c8b7af0b3c5f
Mateusz Ochel <Mateusz.ohel@gmail.com>
2019-01-14 13:18:27 +0100

    add display albums in table

--- display.py
+++ display.py
@@ -1,4 +1,4 @@
-from prettytable import PrettyTable
+
 
 
 def main_menu():
@@ -9,11 +9,20 @@ def main_menu():
     print("5: do you want to find all albums created by given artist")
     print("6: do you want to find album by album name")
     print("7: do you want to get full report in form of set of given statistics")
+    print("8: exit")
+    print('\n')
+#t = PrettyTable(['longest album', 'shortest album', 'oldest album', 'youngest album', 'all albums count + additional info', 'how many albums are given the genres'])
 
 
+def table(albums_list):
+    print(": longest album : shortest album : oldest album : youngest album : all albums count + additional info : how many albums are given the genres :")
 
-
-
-t = PrettyTable([])
-def table():
-    albums_list
+    for item in albums_list:
+        print(":",item[0]," "*(15-len(item[0])),
+        item[1]," "*(18-len(item[1])),
+        item[2]," "*(5-len(item[2])),
+        item[3]," "*(18-len(item[3])),
+        item[4]," "*(13-len(item[4])),)
+        
+        
+        
