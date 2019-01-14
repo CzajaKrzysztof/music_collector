17297200ec0c7e05aeb18211f17b739fd21a7b5e
Mateusz Ochel <Mateusz.ohel@gmail.com>
2019-01-14 12:45:14 +0100

    add main menu display

---
+++ display.py
@@ -0,0 +1,19 @@
+from prettytable import PrettyTable
+
+
+def main_menu():
+    print("1: do you want to view all imported albums")
+    print("2: do you want to find all albums by genre")
+    print("3: do you want to find all albums from given time range")
+    print("4: do you want to find shortest/longest album")
+    print("5: do you want to find all albums created by given artist")
+    print("6: do you want to find album by album name")
+    print("7: do you want to get full report in form of set of given statistics")
+
+
+
+
+
+t = PrettyTable([])
+def table():
+    albums_list
