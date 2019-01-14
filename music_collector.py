287b5ed23ffb5a3135376feb0f71960268b99d8e
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-14 12:27:04 +0100

    Add functionality to find album by given condition

--- music_collector.py
+++ music_collector.py
@@ -1,9 +1,14 @@
 import music_reports
 import display
 
+
 def main():
     albums_list = music_reports.import_data_from_file()
-    print(albums_list)
+    music_reports.temp_print(albums_list)
+    print("\n\n")
+    albums_by_genre = music_reports.find_albums_by_condition(albums_list, 'rock', 3)
+    music_reports.temp_print(albums_by_genre)
+
 
 if __name__ == '__main__':
     main()
--- music_reports.py
+++ music_reports.py
@@ -1,9 +1,21 @@
 def import_data_from_file(filename='text_albums_data.txt'):
-    albums_dictionary = {}
     content = []
-    key = 1
     with open(filename, 'r') as my_file:
         for line in my_file:
             content.append(line.rstrip('\n').split(','))
 
     return content
+
+
+def find_albums_by_condition(albums_list, condition, index):
+    filtered_albums = []
+    for album in albums_list:
+        if condition in album[index]:
+            filtered_albums.append(album)
+
+    return filtered_albums
+
+
+def temp_print(list):
+    for i in list:
+        print(i)
