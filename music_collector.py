44eaa5278e7fce67d9337fd70590373e48bc2bf9
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-14 13:35:56 +0100

    Add function to find albums between given dates

--- music_collector.py
+++ music_collector.py
@@ -1,13 +1,38 @@
 import music_reports
 import display
+import os
 
 
 def main():
+    os.system('clear')
     albums_list = music_reports.import_data_from_file()
-    music_reports.temp_print(albums_list)
-    print("\n\n")
-    albums_by_genre = music_reports.find_albums_by_condition(albums_list, 'rock', 3)
-    music_reports.temp_print(albums_by_genre)
+    display.main_menu()
+    while True:
+        user_chosen_action = music_reports.get_user_action()
+        if user_chosen_action == 1:
+            os.system('clear')
+            display.main_menu()
+            display.table(albums_list)
+        elif user_chosen_action == 2:
+            chosen_genre = music_reports.user_chosen_genre()
+            albums_by_genre = music_reports.find_albums_by_condition(albums_list, chosen_genre, 3)
+            os.system('clear')
+            display.main_menu()
+            display.table(albums_by_genre)
+        elif user_chosen_action == 3:
+            time = music_reports.get_dates_to_filter()
+            os.system('clear')
+            display.main_menu()
+            albums_between_time = music_reports.albums_from_given_time_range(albums_list, *time)
+            display.table(albums_between_time)
+        elif user_chosen_action == 8:
+            break
+        
+        
+        albums_by_genre = music_reports.find_albums_by_condition(albums_list, 'rock', 3)
+        time_start = '1973'
+        time_end = '1976'
+        
 
 
 if __name__ == '__main__':
--- music_reports.py
+++ music_reports.py
@@ -16,6 +16,29 @@ def find_albums_by_condition(albums_list, condition, index):
     return filtered_albums
 
 
-def temp_print(list):
-    for i in list:
-        print(i)
+def albums_from_given_time_range(albums_list, time_start, time_end):
+    filtered_albums = []
+    for album in albums_list:
+        if album[2] >= time_start and album[2] <= time_end:
+            filtered_albums.append(album)
+
+    return filtered_albums
+
+
+def get_user_action():
+    user_action = int(input('What do you want to do: '))
+
+    return user_action
+
+
+def user_chosen_genre():
+    user_genre = input('What music ganre you want to filter albums by: ')
+
+    return user_genre
+
+
+def get_dates_to_filter():
+    time_start = input('Enter start date: ')
+    time_ent = input('Enter end date: ')
+
+    return time_start, time_ent
