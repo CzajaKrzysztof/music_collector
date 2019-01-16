0bb5ba249854d619b880f6fc5946eda1e6695400
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-16 14:23:22 +0100

    Minor pep8 cleaning

--- display.py
+++ display.py
@@ -37,12 +37,17 @@ def show_table(albums_list, table_title, sugestion='no'):
         genre_length = music_reports.get_max_column_length(albums_list, 3) + padding
         time_length = music_reports.get_max_column_length(albums_list, 4) + padding
 
-        print('\n{:^{table_length}}\n'.format(table_title, table_length = (lp_length + artis_length + title_length + year_length + genre_length + time_length + 17)))
-        print("{:^{width_lp}} | {:^{width_artist}s} | {:^{width_title}s} | {:^{width_year}} | {:^{width_genre}s} | {:^{width_time}s} |".format('lp.', 'Artist', 'Title', 'Year', 'Genre', 'Length', width_lp=lp_length, width_artist=artis_length, width_title=title_length, width_year=year_length, width_genre=genre_length, width_time=time_length))
+        print('\n{:^{table_length}}\n'.format(table_title, table_length=(lp_length + artis_length + title_length
+                + year_length + genre_length + time_length + 17)))
+        print("{:^{width_lp}} | {:^{width_artist}s} | {:^{width_title}s} | {:^{width_year}} | {:^{width_genre}s} |
+                {:^{width_time}s} |".format('lp.', 'Artist', 'Title', 'Year', 'Genre', 'Length', width_lp=lp_length,
+                width_artist=artis_length, width_title=title_length, width_year=year_length, width_genre=genre_length, width_time=time_length))
 
         print('-' * (lp_length + artis_length + title_length + year_length + genre_length + time_length + 17))
         for item in albums_list:
-            print("{:^{width_lp}} | {:^{width_artist}s} | {:^{width_title}s} | {:^{width_year}} | {:^{width_genre}s} | {:^{width_time}s} |".format(i, item[0], item[1], item[2], item[3], item[4], width_lp=lp_length, width_artist=artis_length, width_title=title_length, width_year=year_length, width_genre=genre_length, width_time=time_length))
+            print("{:^{width_lp}} | {:^{width_artist}s} | {:^{width_title}s} | {:^{width_year}} | {:^{width_genre}s} |
+                    {:^{width_time}s} |".format(i, item[0], item[1], item[2], item[3], item[4], width_lp=lp_length,
+                    width_artist=artis_length, width_title=title_length, width_year=year_length, width_genre=genre_length, width_time=time_length))
             i += 1
 
 
