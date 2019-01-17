9eab2eb27399b37b16d8e359816208b434bf3213
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-17 11:30:09 +0100

    Refactor display functions

--- display.py
+++ display.py
@@ -5,9 +5,9 @@ def main_menu():
     print('MAIN MENU:')
     print("1: view all imported albums")
     print("2: find all albums by genre")
-    print("3: find all albums from given time range")
+    print("3: find all albums from time range")
     print("4: find shortest/longest album")
-    print("5: find all albums created by given artist")
+    print("5: find all albums created by artist")
     print("6: find album by title")
     print("7: collection statistics")
     print("8: add new album")
@@ -30,20 +30,22 @@ def show_table(albums_list, table_title, sugestion='no'):
     else:
         i = 1
         padding = 3
-        lp_length = 4
-        artis_length = music_reports.get_max_column_length(albums_list, 0) + padding
-        title_length = music_reports.get_max_column_length(albums_list, 1) + padding
-        year_length = music_reports.get_max_column_length(albums_list, 2) + padding
-        genre_length = music_reports.get_max_column_length(albums_list, 3) + padding
-        time_length = music_reports.get_max_column_length(albums_list, 4) + padding
+        lp_len = 4
+        artis_len = music_reports.get_max_column_length(albums_list, 0) + padding
+        title_len = music_reports.get_max_column_length(albums_list, 1) + padding
+        year_len = music_reports.get_max_column_length(albums_list, 2) + padding
+        genre_len = music_reports.get_max_column_length(albums_list, 3) + padding
+        time_len = music_reports.get_max_column_length(albums_list, 4) + padding
 
-        print('\n{:^{table_length}}\n'.format(table_title, table_length=(lp_length + artis_length + title_length
-                + year_length + genre_length + time_length + 17)))
-        print("{:^{width_lp}} | {:^{width_artist}s} | {:^{width_title}s} | {:^{width_year}} | {:^{width_genre}s} | {:^{width_time}s} |".format('lp.', 'Artist', 'Title', 'Year', 'Genre', 'Length', width_lp=lp_length, width_artist=artis_length, width_title=title_length, width_year=year_length, width_genre=genre_length, width_time=time_length))
+        print('\n{:^{table_length}}\n'.format(table_title, table_length=(lp_len + artis_len + title_len
+                + year_len + genre_len + time_len + 17)))
+        header_string = "{:^{w_lp}} | {:^{w_artist}s} | {:^{w_title}s} | {:^{w_year}} | {:^{w_genre}s} | {:^{w_time}s} |"
+        print(header_string.format('lp.', 'Artist', 'Title', 'Year', 'Genre', 'Length', w_lp=lp_len, w_artist=artis_len, w_title=title_len, w_year=year_len, w_genre=genre_len, w_time=time_len))
 
-        print('-' * (lp_length + artis_length + title_length + year_length + genre_length + time_length + 17))
+        print('-' * (lp_len + artis_len + title_len + year_len + genre_len + time_len + 17))
         for item in albums_list:
-            print("{:^{width_lp}} | {:^{width_artist}s} | {:^{width_title}s} | {:^{width_year}} | {:^{width_genre}s} | {:^{width_time}s} |".format(i, item[0], item[1], item[2], item[3], item[4], width_lp=lp_length, width_artist=artis_length, width_title=title_length, width_year=year_length, width_genre=genre_length, width_time=time_length))
+            album_string = "{:^{w_lp}} | {:^{w_artist}s} | {:^{w_title}s} | {:^{w_year}} | {:^{w_genre}s} | {:^{w_time}s} |"
+            print(album_string.format(i, *item, w_lp=lp_len, w_artist=artis_len, w_title=title_len, w_year=year_len, w_genre=genre_len, w_time=time_len))
             i += 1
 
 
